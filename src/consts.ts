03bf7037ecdca97fb7f7567c16344e16e13ab082
Eric Durr <eric.durr@system73.com>
2023-12-21 15:27:14 +0000

    created carta

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Eric Dürr Sierra";
+export const SITE_DESCRIPTION =
+  "Queridos Reyes Magos, este año no me he portado bien pero ...";
